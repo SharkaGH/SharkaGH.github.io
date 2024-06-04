800413d23a998f64da9a3dbe5c04dc26aba7efcf
SharkaGH <jmeno.sarka@gmail.com>
2024-06-04 18:51:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 
   var startWrite = setTimeout(function(){
+    
        var $WIP = "Work in progress...";
-               function initWIP(i){
-                  $("#wip").addClass("sign cursor").text($WIP.substring(0, i));
-                     if(i < $WIP.length){
+    
+        function initWIP(i){
+              $("#wip").addClass("writeOut").text($WIP.substring(0, i));
+                  if(i < $WIP.length){
                         setTimeout(function(){
                            initWIP(i + 1);   
                         }, 40);
-                     }else{/* after finishing do this
+                     }else{
+                    /* after finishing do this
                         setTimeout(function() {
 
-                        }, 1000);*/   
+                        }, 1000);
+                    */   
                      }
-               }
+         }
+    
                setTimeout(function() {
                   initWIP(0);
-               }, 1000);
-            }   
+               }, 1000); 
    }, 2500);
 
 startWrite();
