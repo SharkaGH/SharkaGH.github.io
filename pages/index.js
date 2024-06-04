b48db3c21e3ed6f2de7ac3ae4512a5040ced33f5
SharkaGH <jmeno.sarka@gmail.com>
2024-06-04 18:32:04 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+
+  var startWrite = setTimeout(function(){
+       var $WIP = "Work in progress...";
+               function initWIP(i){
+                  $("#wip").addClass("sign cursor").text($WIP.substring(0, i));
+                     if(i < $WIP.length){
+                        setTimeout(function(){
+                           initWIP(i + 1);   
+                        }, 40);
+                     }else{/* after finishing do this
+                        setTimeout(function() {
+
+                        }, 1000);*/   
+                     }
+               }
+               setTimeout(function() {
+                  initWIP(0);
+               }, 1000);
+            }   
+   }, 2500);
+
+startWrite();
